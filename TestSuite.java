600de9482b7338bc5d45b2429e613d41d0863c30
Franky Cheung <franky.cheung456@gmail.com>
2011-11-18 13:54:50 -0700

    no errors so far on test Sweep :) except for BaseDbmsTest and UserDbmsTest which have not been completed

--- TestSuite.java
+++ TestSuite.java
@@ -2,11 +2,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import DBMS.*;
 import FileManagement.*;
 import Management.*;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({DirectoryManagerTest.class, FileManagerTest.class,CourseAssignmentTest.class,StudentAccessTest.class,
-	DueDateTest.class,CourseManagerTest.class})
+	DueDateTest.class,CourseManagerTest.class,AssignmentDbmsTest.class,BaseDbmsTest.class,InstructorDbmsTest.class,
+	StudentDbmsTest.class,TADbmsTest.class,UserDbmsTest.class,UserAssignmentDbmsTest.class})
 public class TestSuite {
   //nothing
 }
